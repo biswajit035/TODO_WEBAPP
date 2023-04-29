621eb6cee3965a1506e43601144e79c6d022c157
Biswajit Debnath <biswajit.basanti@gmail.com>
2023-04-29 21:32:02 +0530

    feat: :sparkles: logout added
    loggout features added

--- client/src/components/Topbar.jsx
+++ client/src/components/Topbar.jsx
@@ -1,17 +1,27 @@
 import React from 'react'
 import { FaUser } from 'react-icons/fa';
+import { TbLogout } from 'react-icons/tb';
+import { useNavigate } from "react-router-dom";
 
 const Topbar = () => {
+    const navigate = useNavigate()
+    const handleLogout=()=>{
+        localStorage.clear();
+        window.location.reload()
+    }
     return (
         <div className='flex justify-between'>
             <span className='text-xl md:text-2xl font-bold'>
                 Task<span className='text-gray-400'>Manager</span>
             </span>
-            
+
             {/* profile section */}
             <div className='flex items-center gap-1'>
                 <FaUser />
                 Biswajit
+                <TbLogout
+                    style={{ fontSize: "20px", fontWeight: "bold", marginLeft: "10px" }}
+                    onClick={handleLogout} />
             </div>
         </div>
     )
